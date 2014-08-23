b87eda64448b96be67fa2b8c1d52fcf467b99b50
tn00gm <tn00gm@gmail.com>
2014-08-24 00:12:50 +0900

    add script to install golang

---
+++ 08-go.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+readonly TAR_URL="https://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz"
+
+if which go ; then
+    echo "golang already exists."
+    exit 0
+fi
+
+wget -nc $TAR_URL -P /tmp
+sudo tar xzvf /tmp/`basename $TAR_URL` -C /opt
+
+#
+# .profile
+#    export GOROOT="/opt/go"
+#    export PATH="$PATH:$GOROOT/bin"
+#
+
+# end of file

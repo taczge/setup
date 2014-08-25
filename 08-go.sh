42b509514ec5e97869cbea0995fe30303bc8e946
tn00gm <tn00gm@gmail.com>
2014-08-25 21:48:57 +0900

    modify command existence check with if [ -x ... ]

--- 08-go.sh
+++ 08-go.sh
@@ -2,7 +2,7 @@
 
 readonly TAR_URL="https://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz"
 
-if which go ; then
+if [ -x `which go` ]; then
     echo "golang already exists."
     exit 0
 fi

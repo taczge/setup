9d8d072dfaa4ea2fa1607843233cebc2a9c1524d
tn00gm <tn00gm@gmail.com>
2014-09-03 12:43:12 +0900

    modify to change owner to root

--- 07-peco.sh
+++ 07-peco.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
-#
-# configuration ----------------------------------------------------------------
-#
-readonly BIN_URL="https://github.com/peco/peco/releases/download/v0.2.5/peco_linux_amd64.tar.gz"
-readonly BIN_DEST="/usr/local/bin"
-#
-#-------------------------------------------------------------------------------
-#
-
+readonly TAR_URL="https://github.com/peco/peco/releases/download/v0.2.5/peco_linux_amd64.tar.gz"
 if [ -x "`which peco`" ]; then
     echo "[exit] peco is already exists."
     exit 0;
 fi
 
-readonly TAR_FILE="`basename $BIN_URL`"
+wget -nc $TAR_URL -P /tmp
 
-cd /tmp
-wget -nc $BIN_URL
-tar xvf $TAR_FILE
-sudo mv "`basename $TAR_FILE .tar.gz`/peco" $BIN_DEST
+readonly TAR_FILE="`basename $TAR_URL`"
+tar xzvf /tmp/$TAR_FILE -C /tmp
+
+readonly BIN="`basename $TAR_FILE .tar.gz`/peco"
+sudo mv /tmp/$BIN /usr/local/bin
+sudo chown root:root /usr/local/bin/peco

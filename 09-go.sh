60049145b4817daef7c26af3026cd963588e8745
tn <tn00gm@gmail.com>
2014-12-08 03:34:02 +0900

    add configuration for go

--- 09-go.sh
+++ 09-go.sh
@@ -10,10 +10,8 @@ fi
 wget -nc $TAR_URL -P /tmp
 sudo tar xzvf /tmp/`basename $TAR_URL` -C /opt
 
-#
-# .profile
-#    export GOROOT="/opt/go"
-#    export PATH="$PATH:$GOROOT/bin"
-#
-
-# end of file
+# godef のインストールに必要
+sudo apt-get install mercurial
+# GOPATH/PATH が設定されていること
+go get -u github.com/nsf/gocode
+go get -u code.google.com/p/rog-go/exp/cmd/godef

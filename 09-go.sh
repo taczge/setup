9719493802cb2186019a1189d76126a2969010f1
tn <tn00gm@gmail.com>
2014-12-11 20:52:19 +0900

    refactoring

--- 09-go.sh
+++ 09-go.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 
-readonly TAR_URL="https://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz"
+readonly TAR_URL="https://storage.googleapis.com/golang/go1.4.linux-amd64.tar.gz"
 
-if [ -x "`which go`" ]; then
-    echo "golang already exists."
-    exit 0
-fi
+install_go() {
+    if [ -x "`which go`" ]; then
+        echo "golang already exists."
+        return
+    fi
 
-wget -nc $TAR_URL -P /tmp
-sudo tar xzvf /tmp/`basename $TAR_URL` -C /opt
+    wget -nc $TAR_URL -P /tmp
+    sudo tar xzvf /tmp/`basename $TAR_URL` -C /opt
+}
 
-# godef のインストールに必要
-sudo apt-get install mercurial
-# GOPATH/PATH が設定されていること
-go get -u github.com/nsf/gocode
-go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u code.google.com/p/go.tools/cmd/goimports
+download_lib() {
+    echo -n "download $1... "
+    go get -u $1 && echo "success."
+}
+
+install_lib() {
+    if [ -z "$GOPATH" ]; then
+        echo 'cannot download, $GOPATH not set.' >&2
+        return
+    fi
+
+    # godef のインストールに必要
+    sudo apt-get install -y mercurial
+
+    download_lib "github.com/nsf/gocode"
+    download_lib "code.google.com/p/rog-go/exp/cmd/godef"
+    download_lib "code.google.com/p/go.tools/cmd/goimports"
+}
+
+install_go
+install_lib

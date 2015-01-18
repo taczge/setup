1927a4e1a54003b2c65b461224db35d93fd1d22e
tn <tn00gm@gmail.com>
2015-01-18 21:21:15 +0900

    change editor from emacs to emacsclient

--- 06-geeknote.sh
+++ 06-geeknote.sh
@@ -6,7 +6,7 @@ cd $WORK_DIR
 git clone git://github.com/VitaliyRodnenko/geeknote.git
 cd geeknote
 sudo python setup.py install
-geeknote settings --editor emacs
+geeknote settings --editor emacsclient
 
 # python の warning 対策
 [ -d $HOME/.python-eggs ] && chmod 755 $HOME/.python-eggs/

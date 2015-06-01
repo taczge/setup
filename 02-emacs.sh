051dccab8a5ec6509565d6f6bd4a0cb558d990fb
tn <tn00gm@gmail.com>
2015-06-02 08:19:49 +0900

    refactoring

--- 02-emacs.sh
+++ 02-emacs.sh
@@ -6,6 +6,9 @@ readonly WORK_DIR="/tmp"
 readonly SRC_DIR="$HOME/usr/src"
 readonly INSTALL_DIR="$HOME/usr/"
 
+readonly COMPRESSED_FILE=$(basename $SRC_URL)
+readonly DECOMPRESSED_FILE=$(basename $COMPRESSED_FILE .tar.xz)
+
 sudo apt-get install -y \
      libgtk-3-dev \
      libxpm-dev \
@@ -16,9 +19,9 @@ sudo apt-get install -y \
 
 wget -nc $SRC_URL -P $WORK_DIR
 mkdir -p $SRC_DIR
-tar Jvxf $WORK_DIR/$(basename $SRC_URL) -C $SRC_DIR
+tar Jvxf $WORK_DIR/$COMPRESSED_FILE -C $SRC_DIR
 
-cd $SRC_DIR/$(basename $SRC_URL .tar.xz)
+cd $SRC_DIR/$DECOMPRESSED_FILE
 ./configure --prefix=$INSTALL_DIR
 make
 make install

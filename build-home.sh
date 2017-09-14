caebee141d910543bd4ae16d7d3206278d5280ec
Takuma Nagai <tn00gm@gmail.com>
2017-09-14 22:20:40 +0900

    Modify script to reate /home/nagait/{tmp,work}.

--- build-home.sh
+++ build-home.sh
@@ -5,18 +5,17 @@ set -eu
 #
 # $HOME
 #  |- bin ........... (from github)
+#  |- etc
+#  |   |- dot ....... (from github)
+#  |   |- setup ..... (from github)
+#  |   +- exports
 #  |- opt
-#  +- etc
-#      |- dot ....... (from github)
-#      |- setup ..... (from github)
-#      +- exports
+#  |- tmp
+#  +- work
 
 # ~/bin
 cd $HOME
 [ ! -d $HOME/bin ] && git clone https://github.com/taczge/bin.git
-
-# ~/opt
-mkdir -p $HOME/opt
 
 # ~/etc
 mkdir -p $HOME/etc
@@ -24,3 +23,6 @@ cd $HOME/etc
 [ ! -d dotfiles ] && git clone https://github.com/taczge/dotfiles.git
 [ ! -d setup ] && git clone https://github.com/taczge/setup.git
 mkdir exports
+
+# other
+mkdir -p $HOME/{opt,tmp,work}

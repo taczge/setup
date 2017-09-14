ed83efbd689c9108d45829308edc59df30979d91
Takuma Nagai <tn00gm@gmail.com>
2017-09-14 18:48:50 +0900

    Add install script for STS.

---
+++ 12-sts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+readonly SRC_URL="http://download.springsource.com/release/STS/3.9.0.RELEASE/dist/e4.7/spring-tool-suite-3.9.0.RELEASE-e4.7.0-linux-gtk-x86_64.tar.gz"
+readonly INSTALL_DIR="$HOME/opt"
+
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+wget -nc $SRC_URL
+
+readonly COMPRESSED_FILE="$(basename $SRC_URL)"
+readonly EXTRACTED_DIR="$INSTALL_DIR/$(basename $SRC_URL .tar.gz)"
+mkdir -p $EXTRACTED_DIR
+tar vxzf $COMPRESSED_FILE -C $EXTRACTED_DIR
+
+readonly STS_PATH="$EXTRACTED_DIR/sts-bundle/$(echo $COMPRESSED_FILE | sed -e 's@^spring-tool-suite-\(.*RELEASE\).*@sts-\1@g')/STS"
